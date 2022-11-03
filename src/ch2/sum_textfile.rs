ad0e4db116b557d2978b1b6a1c86dd0a65c00ff2
TK-Linux <74335372+TKLinux966@users.noreply.github.com>
2022-11-03 21:34:45 +0900

    Update sum_textfile.rs

--- src/ch2/sum_textfile.rs
+++ src/ch2/sum_textfile.rs
@@ -3,7 +3,7 @@ use std::{env, fs};
 fn main() {
     // コマンドライン引数を得る --- (*1)
     let args = env::args();
-    let mut total:f64 = 0.0;
+    let mut total:usize = 0;
     // 全てのコマンドライン引数を処理 --- (*2)
     for (i, fname) in args.enumerate() {
         if i == 0 { continue; }
@@ -14,9 +14,9 @@ fn main() {
         // 繰り返し加算 --- (*5)
         for line in lines {
             // 数値に変換 --- (*6)
-            let n:f64 = match line.parse() {
+            let n: usize = match line.trim().parse() {
                 Ok(v) => v,
-                Err(_) => 0.0,
+                Err(_) => 0,
             };
             total += n;
         }
@@ -24,5 +24,3 @@ fn main() {
     // 結果を表示 --- (*7)
     println!("{}", total);
 }
-
-
